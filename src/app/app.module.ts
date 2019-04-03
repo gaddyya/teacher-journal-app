6155ff830111e13ad165e11b2e6bca5b02dee4a3
gaddyya <gaddyya@gmail.com>
2019-04-03 14:28:01 +0300

    fix: - change tsling.json for our project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { StudentComponent } from './student/student.component';
-import { GitComponent } from './git/git.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    StudentComponent,
-    GitComponent
   ],
   imports: [
     BrowserModule,
