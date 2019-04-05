2d4b0e38f3edd106d4246cb0d5a63b23b15c7265
gaddyya <gaddyya@gmail.com>
2019-04-05 13:09:49 +0300

    "fix: - create component in student and subject"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './root/app.component';
+import { StudentFormComponent } from './components/students/student-form/student-form.component';
+import { StudentPageComponent } from './components/students/student-page/student-page.component';
+import { StudentTableComponent } from './components/students/student-table/student-table.component';
+import { SubjectFormComponent } from './components/subjects/subject-form/subject-form.component';
+import { SubjectPageComponent } from './components/subjects/subject-page/subject-page.component';
+import { SubjectTableComponent } from './components/subjects/subject-table/subject-table.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    StudentFormComponent,
+    StudentPageComponent,
+    StudentTableComponent,
+    SubjectFormComponent,
+    SubjectPageComponent,
+    SubjectTableComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/subjects/subject-table/subject-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-subject-table',
+  templateUrl: './subject-table.component.html',
+  styleUrls: ['./subject-table.component.sass']
+})
+export class SubjectTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
