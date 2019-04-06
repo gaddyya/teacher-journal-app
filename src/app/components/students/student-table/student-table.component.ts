7a18ccf9350245ba95964704280f13d9cf665b64
Roman Dan <gaddyya@gmail.com>
2019-04-06 16:00:56 +0300

    fix: - table get info from my data

--- src/app/components/students/student-table/student-table.component.ts
+++ src/app/components/students/student-table/student-table.component.ts
@@ -1,23 +1,17 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
 import {MatSort, MatTableDataSource} from '@angular/material';
+import Student from '../../../data/students.json'
 
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
+export interface IStudents {
+  id: number;
+  firstName: string;
+  lastName: string;
+  adress: string;
+  description: string;
 }
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-];
+const STUDENTS: IStudents[] = Student;
+
 @Component({
   selector: 'app-student-table',
   templateUrl: './student-table.component.html',
@@ -25,8 +19,8 @@ const ELEMENT_DATA: PeriodicElement[] = [
 })
 export class StudentTableComponent implements OnInit {
 
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = new MatTableDataSource(ELEMENT_DATA);
+  displayedColumns: string[] = ['id', 'firstName', 'lastName', 'adress', 'description'];
+  dataSource = new MatTableDataSource(STUDENTS);
 
   @ViewChild(MatSort) sort: MatSort;
 
